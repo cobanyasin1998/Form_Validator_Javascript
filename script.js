1424d1e1bcb2124d8e70efe0e33c89f8770bf4ac
Yasin Çoban <yasincoban1998@outlook.com>
2022-08-22 22:07:53 +0300

    first_init

---
+++ script.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("form");
+
+const username = document.getElementById("username");
+
+const email = document.getElementById("email");
+
+const password = document.getElementById("password");
+
+const repassword = document.getElementById("repassword");
+
+form.addEventListener("submit",function(e){
+
+    
+
+});
